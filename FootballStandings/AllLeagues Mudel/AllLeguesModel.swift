16d963043c40d5864510dd5ef8f6934f1d25ef7f
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-07-13 00:21:46 +0300

    Model

--- FootballStandings/AllLeagues Mudel/AllLeguesModel.swift
+++ FootballStandings/AllLeagues Mudel/AllLeguesModel.swift
@@ -7,3 +7,20 @@
 
 import Foundation
 
+struct AllLeguesModel: Decodable {
+    let status: Bool
+    let data: [LegueModel]
+}
+
+struct LegueModel: Decodable {
+    let id: String
+    let name: String
+    let slug: String
+    let abbr: String
+    let logos: LeagueLogos
+}
+
+struct LeagueLogos: Decodable {
+    let light: String
+    let dark: String
+}
